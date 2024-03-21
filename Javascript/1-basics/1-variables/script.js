9a4cca570115ad885594913a32d8832fa74bb926
Joshua Gonzales <codingjogo@gmail.com>
2024-03-21 10:41:22 +0800

    added some files in 1-variable

---
+++ Javascript/1-basics/1-variables/script.js
@@ -0,0 +1,8 @@
+function blabla() {
+    const myObject = ['array1', 'array2', 'array3'];
+    myObject.push('string')
+    
+    return myObject // ['array1', 'array2', 'array3']
+}
+
+console.log(blabla());
